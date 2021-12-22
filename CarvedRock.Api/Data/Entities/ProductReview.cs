e8b322b2b54235a26e6b78fd26731ea099505fd0
Robin Bette <robinbette@hotmail.com>
2021-12-22 11:18:18 +0100

    Add Product reviews

---
+++ CarvedRock.Api/Data/Entities/ProductReview.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarvedRock.Api.Data.Entities
+{
+    public class ProductReview
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+        [StringLength(200), Required]
+        public int Title { get; set; }
+        public int Review { get; set; }
+    }
+}
