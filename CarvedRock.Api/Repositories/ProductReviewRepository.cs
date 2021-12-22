5b7c32b329c077534d8e7db61a0c7b6b8e1c30d4
Robin Bette <robinbette@hotmail.com>
2021-12-22 11:45:46 +0100

    * Add product review type for GQL * Add it to product type * Add product review repo * Add it to Startup

---
+++ CarvedRock.Api/Repositories/ProductReviewRepository.cs
@@ -0,0 +1,24 @@
+﻿using CarvedRock.Api.Data;
+using CarvedRock.Api.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarvedRock.Api.Repositories
+{
+    public class ProductReviewRepository
+    {
+        private readonly CarvedRockDbContext _dbContext;
+
+        public ProductReviewRepository(CarvedRockDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<ProductReview>> GetForProduct(int productId)
+        {
+            return await _dbContext.ProductReviews.Where(pr => pr.ProductId == productId).ToListAsync();
+        }
+    }
+}
