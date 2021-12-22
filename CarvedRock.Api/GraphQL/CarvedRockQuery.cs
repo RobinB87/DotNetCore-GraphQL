a2e62b6cbf7a1247cd08e8fc3f74625f7dd3d576
Robin Bette <robinbette@hotmail.com>
2021-12-22 13:49:14 +0100

    Add some explanation on querying

--- CarvedRock.Api/GraphQL/CarvedRockQuery.cs
+++ CarvedRock.Api/GraphQL/CarvedRockQuery.cs
@@ -8,6 +8,12 @@ namespace CarvedRock.Api.GraphQL
     {
         // GraphQL.NET takes care of the awaiting, so you do not have to do that
         // It also takes care of the conversion of your entity type to the GraphQL type
+
+        // Can use Postman:
+        // POST https://localhost:5001/graphql 
+        // In the body use e.g.: { "query": "{ products { id name price rating photoFileName } }" }
+        // The GraphQL playground will give give nice intellisense though
+        // You can also use { results: products { name photo: photoFileName } } to change names (hence make consumption more easy)
         public CarvedRockQuery(ProductRepository productRepository)
         {
             // List
@@ -23,6 +29,10 @@ namespace CarvedRock.Api.GraphQL
             // Express that it is required by NonNullGraphType
             //
             // Query ex: { product(id:10) { name } } 
+            //
+            // Compare two products by using aliases { p10: product(id: 10) { name, price } p11: product(id: 11) { name, price } } 
+            // Declare fragments to reduce typing queries, e.g.: fragment comparisonFields on ProductType { name, price, stock }
+            //  and then { p10: product(id: 10) { ...comparisonFields } p11: product(id: 11) { ...comparisonFields } }
             Field<ProductType>(
                 "product",
                 arguments: new QueryArguments(new QueryArgument<NonNullGraphType<IdGraphType>>
