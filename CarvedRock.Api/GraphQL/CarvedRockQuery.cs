2ae6e8c2dd5914e821ef48afec5c03f2adf2547f
Robin Bette <robinbette@hotmail.com>
2021-12-22 13:22:54 +0100

    * Add option to get one product by adding QueryArgument

--- CarvedRock.Api/GraphQL/CarvedRockQuery.cs
+++ CarvedRock.Api/GraphQL/CarvedRockQuery.cs
@@ -4,15 +4,34 @@ using GraphQL.Types;
 
 namespace CarvedRock.Api.GraphQL
 {
-    public class CarvedRockQuery: ObjectGraphType
+    public class CarvedRockQuery : ObjectGraphType
     {
         // GraphQL.NET takes care of the awaiting, so you do not have to do that
         // It also takes care of the conversion of your entity type to the GraphQL type
         public CarvedRockQuery(ProductRepository productRepository)
         {
+            // List
             Field<ListGraphType<ProductType>>(
-                "products", 
+                "products",
                 resolve: context => productRepository.GetAll()
+            );
+
+            // One
+            // Field name is "product"
+            // Use arguments via QueryArguments - here of the IdGraphType -> expects id of product
+            // You can use any graph type you want as arg
+            // Express that it is required by NonNullGraphType
+            //
+            // Query ex: { product(id:10) { name } } 
+            Field<ProductType>(
+                "product",
+                arguments: new QueryArguments(new QueryArgument<NonNullGraphType<IdGraphType>>
+                { Name = "id" }),
+                resolve: context =>
+                {
+                    var id = context.GetArgument<int>("id");
+                    return productRepository.GetOne(id);
+                }
             );
         }
     }
